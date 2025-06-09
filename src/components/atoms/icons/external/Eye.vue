1214644bcd4988de74c23adf8e74493a7da9cf44
Gb16702 <delgeoffrey1@gmail.com>
2025-06-09 20:37:30 +0200

    feat(icon components): add IconWrapper, Eye, EyeOff components

---
+++ src/components/atoms/icons/external/Eye.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+    import IconWrapper, { type IconWrapperProps } from "../internal/IconWrapper.vue";
+    const props = defineProps<IconWrapperProps>();
+</script>
+
+<template>
+    <IconWrapper v-bind="props">
+        <path
+            d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0"
+        />
+        <circle cx="12" cy="12" r="3" />
+    </IconWrapper>
+</template>
---
+++ src/components/atoms/icons/internal/IconWrapper.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+    export type IconWrapperProps = {
+        size?: string;
+        fill?: string;
+        stroke: string;
+        width?: string;
+        classes?: string;
+    };
+
+    withDefaults(defineProps<IconWrapperProps>(), {
+        size: "16",
+        fill: "none",
+        stroke: "currentColor",
+        width: "2",
+        classes: "",
+    });
+</script>
+
+<template>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        :width="size"
+        :height="size"
+        viewBox="0 0 24 24"
+        :fill="fill"
+        :stroke="stroke"
+        :stroke-width="width"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        :class="classes"
+        role="img"
+    >
+        <slot />
+    </svg>
+</template>
