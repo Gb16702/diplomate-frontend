bad05737377c5941c1bea766e2d0adb0de7f07ed
Gb16702 <delgeoffrey1@gmail.com>
2025-06-09 16:29:05 +0200

    feat(label): add label component and story

---
+++ src/components/atoms/label/Label.stories.ts
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from "@storybook/vue3-vite"
+import Label from "./Label.vue"
+
+const meta = {
+    title: "Atoms/Label",
+    component: Label,
+    tags: ["autodocs"],
+    parameters: {
+        docs: {
+            description: {
+                component: "Form Label"
+            }
+        }
+    }
+} satisfies Meta<typeof Label>
+
+export default meta;
+
+export const Base = {
+    argTypes: {
+        required: {
+            control: "boolean",
+            description: "Whether the label is required or not"
+        },
+        inputId: {
+            control: "text",
+            description: "What input is this label linked to ?"
+        },
+        default: {
+            control: "text",
+            description: "Whether the label is required or not"
+        }
+    },
+
+    args: {
+        required: false,
+        inputId: "username",
+        default: "username",
+    }
+} satisfies StoryObj<typeof Label>
