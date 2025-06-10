e792cf447e7eaff009841483b41742d359d4a027
Gb16702 <delgeoffrey1@gmail.com>
2025-06-10 23:17:34 +0200

    feat(components): add input component and story

---
+++ src/components/molecules/input/Input.vue
@@ -0,0 +1,83 @@
+<script setup lang="ts">
+  import { ref, computed } from "vue";
+  import EyeOff from "../../atoms/icons/external/EyeOff.vue";
+  import Eye from "../../atoms/icons/external/Eye.vue";
+
+  type InputProps = {
+    id?: string;
+    type?: string;
+    placeholder?: string;
+    isDisabled?: boolean;
+  }
+
+  const showPassword = ref(false);
+
+  const props = withDefaults(defineProps<InputProps>(), {
+    type: "text"
+  })
+
+  const isPassword = computed(() => props.type === "password");
+  const isPasswordVisible = computed(() => {
+    return isPassword.value ? (showPassword.value ? "text" : "password") : props.type;
+  })
+</script>
+
+<template>
+  <div class="form-group-wrapper">
+    <div class="input-wrapper">
+      <input
+        :type="isPasswordVisible"
+        :id="props.id"
+        :name="props.id"
+        :disabled="props.isDisabled"
+        :placeholder="props.placeholder"
+        :class="{'isPasswordInput': isPassword}"
+        v-bind="$attrs"
+      />
+      <button
+          v-if="isPassword"
+          type="button"
+          class="password-input-type-toggle"
+          @click="showPassword = !showPassword"
+          :tabindex="-1"
+      >
+        <Eye v-if="showPassword" :size="16" />
+        <EyeOff v-else :size="16" />
+      </button>
+    </div>
+  </div>
+</template>
+
+<style>
+  .input-wrapper {
+    position: relative;
+    width: 350px;
+    height: 44px;
+    border-radius: 50px;
+    overflow: hidden;
+    outline: 1px solid var(--color-gray-light);
+    background-color: white;
+    display: flex;
+    align-items: center;
+    justify-content: start;
+    transition: outline .15s ease;
+
+    &:focus-within {
+      outline: 1px solid var(--color-orange);
+    }
+
+    &:has(input:disabled) {
+      background-color: var(--color-gray-light);
+    }
+
+    button {
+      position: absolute;
+      right: 12px;
+      top: 50%;
+      transform: translateY(-50%);
+      color: var(--color-gray-light);
+      background-color: transparent;
+      padding: 7px;
+    }
+  }
+</style>
---
+++ src/components/molecules/input/input.stories.ts
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from "@storybook/vue3-vite"
+import Input from "./Input.vue"
+
+const meta = {
+    title: "Molecules/Input",
+    component: Input,
+    parameters: {
+        docs: {
+            description: {
+                component: "Customizable input"
+            }
+        }
+    }
+} satisfies Meta<typeof Input>
+
+export default meta;
+
+export const Base = {
+    argTypes: {
+        id: {
+            control: "text",
+            description: "Label content"
+        },
+        type: {
+            control: "select",
+            options: ["text", "email", "password"],
+            description: "Input type"
+        },
+        placeholder: {
+            control: "text",
+            description: "Input placeholder"
+        },
+        isDisabled: {
+            control: "boolean",
+            description: "Whether the input is disabled or not",
+        }
+    },
+    args: {
+        id: "username",
+        type: "text",
+        placeholder: "socrates@gmail.com",
+        isDisabled: false,
+    }
+} satisfies StoryObj<typeof Input>
