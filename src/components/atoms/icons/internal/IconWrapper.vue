6d174c6b1d2b254bbc08fad043d6cc8345efe946
Gb16702 <delgeoffrey1@gmail.com>
2025-06-10 23:18:31 +0200

    fix(icons): bind props correctly in icon wrapper

--- src/components/atoms/icons/internal/IconWrapper.vue
+++ src/components/atoms/icons/internal/IconWrapper.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
     export type IconWrapperProps = {
-        size?: string;
+        size?: number;
         fill?: string;
         stroke?: string;
         width?: string;
         classes?: string;
     };
 
-    withDefaults(defineProps<IconWrapperProps>(), {
-        size: "16",
+    const props = withDefaults(defineProps<IconWrapperProps>(), {
+        size: 16,
         fill: "none",
         stroke: "currentColor",
         width: "2",
@@ -19,15 +19,15 @@
 <template>
     <svg
         xmlns="http://www.w3.org/2000/svg"
-        :width="size"
-        :height="size"
+        :width="props.size"
+        :height="props.size"
         viewBox="0 0 24 24"
-        :fill="fill"
-        :stroke="stroke"
-        :stroke-width="width"
+        :fill="props.fill"
+        :stroke="props.stroke"
+        :stroke-width="props.width"
         stroke-linecap="round"
         stroke-linejoin="round"
-        :class="classes"
+        :class="props.classes"
         role="img"
     >
         <slot />
